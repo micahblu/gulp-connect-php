70004ac0b5ebc8bcdafef199bf1d402a425b1dde
Micah Blu <micahblu@gmail.com>
2014-11-13 14:40:43 -0800

    added rough index.js

---
+++ index.js
@@ -0,0 +1,104 @@
+'use strict';
+var spawn = require('child_process').spawn;
+var http = require('http');
+var open = require('opn');
+var binVersionCheck = require('bin-version-check');
+
+module.exports = (function () {
+	var checkServerTries = 0;
+
+	function checkServer(hostname, port, cb) {
+		setTimeout(function () {
+			http.request({
+				method: 'HEAD',
+				hostname: hostname,
+				port: port
+			}, function (res) {
+				var statusCodeType = Number(res.statusCode.toString()[0]);
+
+				if ([2, 3, 4].indexOf(statusCodeType) !== -1) {
+					return cb();
+				} else if (statusCodeType === 5) {
+					console.log(
+						'Server docroot returned 500-level response. Please check ' +
+						'your configuration for possible errors.'
+					);
+					return cb();
+				}
+
+				checkServer(hostname, port, cb);
+			}).on('error', function (err) {
+				// back off after 1s
+				if (++checkServerTries > 20) {
+					return cb();
+				}
+
+				console.log('PHP server not started. Retrying...');
+				checkServer(hostname, port, cb);
+			}).end();
+		}, 50);
+	}
+
+
+	var server = function (options){
+		var cb = function(){
+			console.log('anonymous cb triggered');
+		};
+
+		if(!options){
+			options = {
+				port: 8000,
+				hostname: '127.0.0.1',
+				base: '.',
+				keepalive: false,
+				open: false,
+				bin: 'php'
+			};
+		}
+
+		var host = options.hostname + ':' + options.port;
+		var args = ['-S', host];
+
+		if (options.ini) {
+			args.push('-c', options.ini);
+		}
+
+		if (options.router) {
+			args.push(options.router);
+		}
+
+
+		binVersionCheck(options.bin, '>=5.4', function (err) {
+			if (err) {
+				console.log(err);
+				cb();
+				return;
+			}
+
+			var cp = spawn(options.bin, args, {
+				cwd: options.base,
+				stdio: 'inherit'
+			});
+
+			// quit PHP when grunt is done
+			process.on('exit', function () {
+				cp.kill();
+			});
+
+			// check when the server is ready. tried doing it by listening
+			// to the child process `data` event, but it's not triggered...
+			checkServer(options.hostname, options.port, function () {
+				if (!this.flags.keepalive && !options.keepalive) {
+					cb();
+				}
+
+				if (options.open) {
+					open('http://' + host);
+				}
+			}.bind(this));
+		}.bind(this));
+	};
+	return {
+		server: server
+	}
+})();
