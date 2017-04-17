319cac9ac7d53bc48247652e4369d5e5435650a5
Glenn R. Martin <glenn@intrepid.io>
2017-04-17 09:51:08 -0400

    Adding Systemic Status... Allowing us to have a state machine and tighten up timers. Updating anonymous functions to have names for easier to follow debugging.

--- index.js
+++ index.js
@@ -10,144 +10,175 @@
   const binVersionCheck = require('bin-version-check');
   const fs = require('fs');
 
-  class PhpDevelopmentServerConnection {
-    constructor(opts) {
-      this.checkServerTries = 0;
+  //let counter = 0;
 
-      this.workingPort = 8000;
+  function EnumSet() {
+    [...arguments].forEach((x) => { this[x] = Symbol(x) });
+  }
 
-      this.defaults = Object.assign({
-        port: 8000,
-        hostname: '127.0.0.1',
-        base: '.',
-        open: false,
-        bin: 'php',
-        root: '/',
-        stdio: 'inherit',
-        configCallback: null,
-        debug: false
-      }, opts || {});
-      return this; // `new` bug
-    }
+  const PhpDevelopmentServerConnection = ((function _PhpDevelopmentServerConnection_private_scope() {
+    const Status = new EnumSet('NEW', 'STARTING', 'STARTED', 'FINISHED');
 
-    closeServer(cb) {
-      if (this.childProcess) {
-        cb(this.childProcess.kill('SIGKILL'));
-        return;
-      }
-      cb();
-    }
-
-    get port() { return this.workingPort; }
-
-    checkServer(hostname, port, cb) {
+    function checkServer(hostname, port, cb) {
       const self = this;
-      setTimeout(function () {
+      //console.log(`[${this.counter}] checkServer`);
+
+      if (self.status !== Status.STARTING) return;
+
+      setTimeout(function _checkServer_fire() {
         http.request({
           method: 'HEAD',
           hostname: hostname,
           port: port
-        }, function (res) {
+        }, function _checkServer_httpCallback(res) {
           const statusCodeType = Number(res.statusCode.toString()[0]);
 
           if ([2, 3, 4].indexOf(statusCodeType) !== -1) {
-            return cb();
+            return cb(true);
           } else if (statusCodeType === 5) {
             console.log(
               'Server docroot returned 500-level response. Please check ' +
               'your configuration for possible errors.'
             );
-            return cb();
+            return cb(true);
           }
 
-          //self.checkServer(hostname, port, cb);
-        }).on('error', function (err) {
+          checkServer.call(self, hostname, port, cb);
+        }).on('error', function _checkServer_httpError(err) {
           // back off after 1s
-          if (++this.checkServerTries > 20) {
+          if (++self.checkServerTries > 20) {
             console.log('PHP server not started. Retrying...');
-            return cb();
+            return cb(false);
           }
-          //self.checkServer(hostname, port, cb);
+          checkServer.call(self, hostname, port, cb);
         }).end();
-      }, 50);
+      }, 15);
     }
 
-    server(options, cb) {
-      if (!cb) {
-        cb = function () { };
+    class PhpDevelopmentServerConnection {
+      constructor(opts) {
+        //this.counter = ++counter;
+        //console.log(`[${this.counter}] constructor`);
+
+        this.status = Status.NEW;
+
+        this.checkServerTries = 0;
+
+        this.workingPort = 8000;
+
+        this.defaults = Object.assign({
+          port: 8000,
+          hostname: '127.0.0.1',
+          base: '.',
+          open: false,
+          bin: 'php',
+          root: '/',
+          stdio: 'inherit',
+          configCallback: null,
+          debug: false
+        }, opts || {});
+
+        return this; // `new` bug
       }
 
-      const self = this;
-
-      options = Object.assign({}, this.defaults, options);
-
-      this.workingPort = options.port;
-      const host = options.hostname + ':' + options.port;
-      const args = ['-S', host, '-t', options.base];
-
-      if (options.ini) {
-        args.push('-c', options.ini);
-      }
-
-      if (options.router) {
-        args.push(require('path').resolve(options.router));
-      }
-
-      if (options.debug) {
-        spawn = function (outerSpawn) {
-          return function debugSpawnWrapper(file, args, options) {
-            console.log('Invoking Spawn with:');
-            console.log(file);
-            console.log(args);
-            console.log(options);
-
-            return outerSpawn(file, args, options);
-          }
-        }(spawn);
-      }
-
-      if (options.configCallback === null || options.configCallback === undefined) {
-        options.configCallback = function noOpConfigCallback(type, collection) {
-          return collection;
-        }
-      }
-
-      spawn = function (outerSpawn) {
-        return function configCallbackSpawnWrapper(file, spawnArgs, spawnOptions) {
-          return outerSpawn(file, options.configCallback(OPTIONS_PHP_CLI_ARR, spawnArgs), options.configCallback(OPTIONS_SPAWN_OBJ, spawnOptions));
-        }
-      }(spawn);
-
-
-      binVersionCheck(options.bin, '>=5.4', function (err) {
-        if (err) {
-          cb();
+      closeServer(cb) {
+        //console.log(`[${this.counter}] closeServer`);
+        const self = this;
+        if (this.loading) {
+          setTimeout(() => self.closeServer(cb), 5);
           return;
         }
-        const checkPath = function () {
-          const exists = fs.existsSync(options.base);
-          if (exists === true) {
-            self.childProcess = spawn(options.bin, args, {
-              cwd: '.',
-              stdio: options.stdio
-            });
+
+        if (this.childProcess) {
+          cb(this.childProcess.kill('SIGKILL'));
+          this.status = Status.FINISHED;
+          return;
+        }
+
+        cb();
+      }
+
+      get port() { return this.workingPort; }
+
+      server(options, cb) {
+        //console.log(`[${this.counter}] server`);
+        cb = cb || function _noop() { };
+
+        const self = this;
+
+        options = Object.assign({}, this.defaults, options);
+
+        this.workingPort = options.port;
+        const host = options.hostname + ':' + options.port;
+        const args = ['-S', host, '-t', options.base];
+
+        if (options.ini) {
+          args.push('-c', options.ini);
+        }
+
+        if (options.router) {
+          args.push(require('path').resolve(options.router));
+        }
+
+        if (options.debug) {
+          spawn = function _debugSpawn(outerSpawn) {
+            return function debugSpawnWrapper(file, args, options) {
+              console.log('Invoking Spawn with:');
+              console.log(file);
+              console.log(args);
+              console.log(options);
+
+              return outerSpawn(file, args, options);
+            }
+          }(spawn);
+        }
+
+        if (options.configCallback === null || options.configCallback === undefined) {
+          options.configCallback = function noOpConfigCallback(type, collection) {
+            return collection;
           }
-          else {
-            setTimeout(checkPath, 100);
+        }
+
+        spawn = function _configCallbackSpawn(outerSpawn) {
+          return function configCallbackSpawnWrapper(file, spawnArgs, spawnOptions) {
+            return outerSpawn(file, options.configCallback(OPTIONS_PHP_CLI_ARR, spawnArgs), options.configCallback(OPTIONS_SPAWN_OBJ, spawnOptions));
           }
-        };
-        checkPath();
-        // check when the server is ready. tried doing it by listening
-        // to the child process `data` event, but it's not triggered...
-        self.checkServer(options.hostname, options.port, function () {
-          if (options.open) {
-            open('http://' + host + options.root);
+        }(spawn);
+
+        binVersionCheck(options.bin, '>=5.4', function _binVerCheck(err) {
+          if (err) {
+            cb();
+            return;
           }
-          cb();
+          const checkPath = function _checkPath() {
+            const exists = fs.existsSync(options.base);
+            if (exists === true) {
+              self.status = Status.STARTING;
+              self.childProcess = spawn(options.bin, args, {
+                cwd: '.',
+                stdio: options.stdio
+              });
+            }
+            else {
+              setTimeout(checkPath, 100);
+            }
+          };
+          checkPath();
+          // check when the server is ready. tried doing it by listening
+          // to the child process `data` event, but it's not triggered...
+          checkServer.call(self, options.hostname, options.port, function _server_checkServer() {
+            self.status = Status.STARTED;
+            if (options.open) {
+              open('http://' + host + options.root);
+            }
+            cb();
+          }.bind(this));
         }.bind(this));
-      }.bind(this));
-    };
-  }
+      };
+    }
+
+    return PhpDevelopmentServerConnection;
+  }))();
 
   module.exports = (function _export_scoping() {
 
